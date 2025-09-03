89b5053ded764429c2b653f27a66c50f882d4d3b
Super User <root@DESKTOP-I98R5CS.localdomain>
2025-09-03 10:11:45 +0900

    주소 변경

--- front/src/api/mainPageApi.tsx
+++ front/src/api/mainPageApi.tsx
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // 오늘자 정보 전체 조회 API
 export async function fetchTodayInfo() {
-  const response = await axios.get('http://localhost:8080/api/mongo/latest');
+  const response = await axios.get('/api/mongo/latest');
   return response.data;
 }
