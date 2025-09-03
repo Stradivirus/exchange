8cdebfdda90fcf5c7710913e4ab2903fd8d45ca8
Super User <root@DESKTOP-I98R5CS.localdomain>
2025-09-03 10:15:19 +0900

    테스트

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -25,7 +25,7 @@ const MainPage: React.FC = () => {
 
   return (
     <div>
-      <h1>오늘의 주요 지표</h1>
+      <h1>최신 주요 지표</h1>
       {data.exchange ? <ExchangeSection data={data.exchange} /> : <div>Exchange 데이터 없음</div>}
       {data.stock ? <StockSection data={data.stock} /> : <div>Stock 데이터 없음</div>}
       {data.commodities ? <CommoditiesSection data={data.commodities} /> : <div>Commodities 데이터 없음</div>}
